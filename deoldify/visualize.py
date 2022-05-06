3f35d4f34e61877d4ce0a7f0a9b9956a9e250f6a
sugi-san <90301727+sugi-san@users.noreply.github.com>
2022-05-06 15:24:44 +0900

    Update visualize.py

--- deoldify/visualize.py
+++ deoldify/visualize.py
@@ -15,18 +15,6 @@ from IPython.display import HTML
 from IPython.display import Image as ipythonimage
 import cv2
 
-
-# display_mp4
-def display_mp4(path):
-    from base64 import b64encode
-    mp4 = open(path,'rb').read()
-    data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-    ipythondisplay(HTML("""
-    <video width=700 controls>
-        <source src="%s" type="video/mp4">
-    </video>
-    """ % data_url))
-    #print('Display finished.')  ###
     
 # adapted from https://www.pyimagesearch.com/2016/04/25/watermarking-images-with-opencv-and-python/
 def get_watermarked(pil_image: Image) -> Image:
